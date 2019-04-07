00ce4596beab6bfd7aa83faa55b0dbb74b286f29
nikiforov <nikiforov@selectel.ru>
2019-04-07 23:03:50 +0300

    01. two line helloworld

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "lib.h"
-
-#include <spdlog/spdlog.h>
+#include <iostream>
 
 int main() {
-  auto logger = spdlog::stdout_logger_mt("console");
-  logger->info("version {} was started", version());
+
+  std::cout << "build " << version() << std::endl;
+  std::cout << "Hello, World!";
 
   return 0;
 }
