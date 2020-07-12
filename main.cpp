6add0cba989d5c94d38738a57e8d529643a7e364
Aleksandr Nikiforov <nikiforov@selectel.ru>
2020-07-12 22:59:54 +0300

    some minor updates for new output

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "lib.h"
 #include <iostream>
 
-int main() {
+int main(int, char **) {
 
-  std::cout << "build " << version() << std::endl;
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Version: " << version() << std::endl;
 
   return 0;
 }
